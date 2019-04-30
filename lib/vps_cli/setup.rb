2964dbb1320b4e05bd6aca4a66040205c42394a4
paramagicdev <konnor5456@gmail.com>
2019-04-29 23:38:04 -0400

    modified .gitignore, deprecated methods in setup

--- lib/vps_cli/setup.rb
+++ lib/vps_cli/setup.rb
@@ -3,17 +3,26 @@
 module VpsCli
   # Various setup to include ufw firewalls, adding repos, adding fonts etc
   class Setup
+    # checks if the user has sudo privileges via uid
     def self.privileged_user?
       Process.uid.zero?
     end
 
+    # checks if a user is root
     def self.root?
       privileged_user? && Dir.home == '/root'
     end
 
+    # Runs the full setup process
+    # @see #ufw_setup
+    # @see #add_dejavu_sans_mono_font
+    # @see #add_repos this method is deprecated due to all packages being added
+    #   to latest ubuntu 18.04+
     def self.full
+      # this is here for compatibility purposes, no longer runs anything
       add_repos
       add_dejavu_sans_mono_font
+
       ufw_setup
     end
 
@@ -34,6 +43,10 @@ module VpsCli
       Rake.sh('yes | sudo systemctl restart sshd')
     end
 
+    def self.add_dejavu_sans_mono_font
+      Rake.sh('mkdir -p ~/.local/share/fonts')
+      Rake.sh(%(cd ~/.local/share/fonts && curl -fLo "DejaVu Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.ttf))
+    end
     ##
     # Adds repos to the package manager to be tracked
     # Adds the following repos:
@@ -41,15 +54,15 @@ module VpsCli
     # This method used to add neovim, asciinema, and mosh as well
     # But they are all part of the base ubuntu 18.10 release
     def self.add_repos
-      add_docker_repo
-      add_yarn_repo
-
       ## Now part of cosmic release for Ubuntu 18.10
+      # add_yarn_repo
+      # add_docker_repo
       # add_neovim_repo
       # add_mosh_repo
       # add_asciinema_repo
     end
 
+    # @deprecated Now part of the standard packaging of Ubuntu 18.04+
     def self.add_docker_repo
       # Instructions straight from https://docs.docker.com/install/linux/docker-ce/ubuntu/#set-up-the-repository
       # Docker repo
@@ -61,6 +74,7 @@ module VpsCli
              stable"})
     end
 
+    # @deprecated Now part of the standard packaging of Ubuntu 18.04+
     def self.add_yarn_repo
       # yarn repo
       Rake.sh(%( curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -))
@@ -68,27 +82,24 @@ module VpsCli
       Rake.sh('sudo apt update')
     end
 
-    # @deprecated
+
+    # @deprecated Now part of the standard packaging of Ubuntu 18.04+
     def self.add_neovim_repo
       # add neovim
       Rake.sh('sudo add-apt-repository ppa:neovim-ppa/stable')
     end
 
-    # @deprecated
+    # @deprecated Now part of the standard packaging of Ubuntu 18.04+
     def self.add_mosh_repo
       # mosh repo
       Rake.sh(%(yes "\n" | sudo add-apt-repository ppa:keithw/mosh))
     end
 
-    # @deprecated
+    # @deprecated Now part of the standard packaging of Ubuntu 18.04+
     def self.add_asciinema_repo
       # asciinema repo for recording the terminal
       Rake.sh('sudo apt-add-repository -y ppa:zanchey/asciinema')
     end
 
-    def self.add_dejavu_sans_mono_font
-      Rake.sh('mkdir -p ~/.local/share/fonts')
-      Rake.sh(%(cd ~/.local/share/fonts && curl -fLo "DejaVu Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.ttf))
-    end
   end
 end
