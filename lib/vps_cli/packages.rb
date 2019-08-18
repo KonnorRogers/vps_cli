c469070637e64b87ec1a1309a395bf0f1a949ba3
paramagicdev <konnor5456@gmail.com>
2019-08-18 02:15:20 -0400

    Adding packages for compilation of php

--- lib/vps_cli/packages.rb
+++ lib/vps_cli/packages.rb
@@ -15,7 +15,7 @@ module VpsCli
     LIBS = %w[libssl1.0-dev libcurl4-openssl-dev libxml2-dev
               re2c libssl-dev libbz2-dev libjpeg-turbo8-dev libpng-dev
               libzip-dev libtidy-dev libxslt-dev automake libtool autoconf
-              flex bison].freeze
+              flex bison libkrb5-dev libonig-dev].freeze
 
     GEMS = %w[colorls neovim rake pry
               rubocop gem-ctags rails yard
